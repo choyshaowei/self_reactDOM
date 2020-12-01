2da38b6293fed960e55a43817e2264e119e558b3
choyshaowei <choy950721@gmail.com>
2020-12-01 22:46:59 +0800

    Form Two Way Data Binding
    Method for 2 way data binding for React.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import "./App.css";
+
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  const [courseGoals, setCourseGoals] = useState([
+    { id: "cg1", text: "Finished the Course" },
+    { id: "cg2", text: "All about course" },
+    { id: "cg3", text: "Help other students for QnA" },
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    // setCourseGoals(courseGoals.concat(newGoal))
+    setCourseGoals((precCourseGoals) => {
+      return precCourseGoals.concat(newGoal);
+    });
+    console.log(courseGoals);
+  };
+
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}></NewGoal>
+      <GoalList goals={courseGoals}></GoalList>
+    </div>
+  );
 };
 
 export default App;
