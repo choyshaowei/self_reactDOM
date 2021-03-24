1d3f53a652a2b6bb7cf94e269ae5e8788b61684b
Shao Wei <33794625+choyshaowei@users.noreply.github.com>
2021-03-24 20:56:59 +0800

    Add pagination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import ReactPaginate from "react-paginate";
 
 import "./App.css";
 
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
@@ -11,6 +14,27 @@ const App = () => {
     { id: "cg2", text: "All about course" },
     { id: "cg3", text: "Help other students for QnA" },
   ]);
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setPosts(await res.json());
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
 
   const addNewGoalHandler = (newGoal) => {
     // setCourseGoals(courseGoals.concat(newGoal))
@@ -20,11 +44,34 @@ const App = () => {
     console.log(courseGoals);
   };
 
+  const indexOfLastpost = currentPage * postPerPage;
+  const indeOfFirstPost = indexOfLastpost - postPerPage;
+  const currentPosts = posts.slice(indeOfFirstPost, indexOfLastpost);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div className="course-goals">
-      <h2>Course Goals</h2>
-      <NewGoal onAddGoal={addNewGoalHandler}></NewGoal>
-      <GoalList goals={courseGoals}></GoalList>
+      <h2>
+        <button type="button" class="btn btn-danger btn-lg m-5">
+          Course Goals
+        </button>
+      </h2>
+      {/* <NewGoal onAddGoal={addNewGoalHandler}></NewGoal>
+      <GoalList goals={courseGoals}></GoalList> */}
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossorigin="anonymous"
+      />
+      <div className="container-fluid">
+        <Posts posts={currentPosts} loading={loading}></Posts>
+        <Pagination
+          postsPerPage={postPerPage}
+          totalPosts={posts.length}
+          paginate={paginate}
+        ></Pagination>
+      </div>
     </div>
   );
 };
---
+++ src/components/Pagination.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  //   console.log(postsPerPage)
+  //   console.log(totalPosts)
+  //   console.log(pageNumbers)
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <a onClick={() => paginate(number)} href="!#" className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+
+    // <nav>
+    //   <ul className="pagination">
+    //    <li><a href="!#">Test</a></li>
+    //   </ul>
+    // </nav>
+  );
+};
+
+export default Pagination;
